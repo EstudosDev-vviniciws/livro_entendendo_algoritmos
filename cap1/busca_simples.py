6e737c55b8ce96d91500541f4b41d1610a0aa76c
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-05-07 11:06:22 -0300

    Adicionei a pergunta do exercício sobre Busca Simples

--- cap1/busca_simples.py
+++ cap1/busca_simples.py
@@ -1,3 +1,9 @@
+'''
+Busca Simples:
+Escreva uma função que use busca simples para encontrar um valor em uma
+lista desordenada. A função deve retornar o índice do valor ou -1 se não encontrar.
+'''
+
 def busca_simples(lista,valor):
     for i in range(len(lista)):
         if lista[i] == valor:
