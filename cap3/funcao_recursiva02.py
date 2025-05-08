339140292c6a7b1bf9498bdfce84b7b62964304b
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-05-08 10:44:35 -0300

    Exercício do capítulo 3 sobre Recursão

---
+++ cap3/funcao_recursiva02.py
@@ -0,0 +1,15 @@
+'''
+Caso-base e o Caso recursivo:
+Objetivo: Crie uma função recursiva fatorial(n) que calcule o fatorial de n.
+'''
+
+
+def fatorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fatorial (n-1)
+
+
+n = int(input("Digite um número: "))    
+print(f"O fatorial de {n} é: {fatorial(n)}")
