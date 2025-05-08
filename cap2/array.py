5a8c508ec8360a3693feb3e3cd7bca01616e855f
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-05-08 09:49:26 -0300

    Exercício do capítulo 2 sobre Array

---
+++ cap2/array.py
@@ -0,0 +1,10 @@
+'''
+Acesso em Arrays:
+Objetivo: Demonstre o acesso direto a elementos em um array.
+'''
+
+def acessar_array():
+    numeros = [10, 20, 30, 40, 50, 60]
+    return numeros[2]
+
+print(acessar_array())
