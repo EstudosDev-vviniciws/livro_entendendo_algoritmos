cdc353a1ea55ebd5597a6ecd64d6a6f8d5466395
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-05-19 10:58:23 -0300

    Exercício do capítulo 8 sobre Algoritmos Gulosos

---
+++ cap8/algoritmos_gulosos03.py
@@ -0,0 +1,30 @@
+'''
+Algoritmos Gulosos:
+Objetivo: Escolha o menor número de estações
+que cubram todos os estados necessários.
+'''
+
+estacoes = {
+    'kone': {'id', 'nv', 'ut'},
+    'ktwo': {'wa', 'id', 'mt'},
+    'kthree': {'or', 'nv', 'ca'},
+    'kfour': {'nv', 'ut'},
+    'kfive': {'ca', 'az'}
+}
+
+estados_necessarios = {'mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'}
+
+estados_finais = set()
+
+while estados_necessarios:
+    melhor_estacao = None
+    estados_cobertos = set()
+    for estacao, estados in estacoes.items():
+        cobertos = estados & estados_necessarios
+        if len(cobertos) > len(estados_cobertos):
+            melhor_estacao = estacao
+            estados_cobertos = cobertos
+    estados_necessarios -= estados_cobertos
+    estados_finais.add(melhor_estacao)
+    
+print("Estações escolhidas: ", estados_finais)
