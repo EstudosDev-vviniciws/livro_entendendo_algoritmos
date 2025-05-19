4e7595f1e587020cf7896a3df6be83b031a3e1c0
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-05-19 11:37:52 -0300

    Exercício do capítulo 8 sobre Algoritmos Gulosos

---
+++ cap8/algoritmos_gulosos04.py
@@ -0,0 +1,37 @@
+'''
+Algoritmos Gulosos:
+Objetivo: Você deve visitar todas as cidades uma única vez
+e retornar à cidade de origem. Aqui, uma aproximação gulosa
+é visitar sempre a cidade mais próxima.
+'''
+
+import math
+
+cidades = {
+    'A': (0, 0),
+    'B': (1, 3),
+    'C': (4, 3),
+    'D': (6, 1),
+}
+
+def distancia(p1, p2):
+    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+
+def rota_mais_proxima(cidades):
+    nao_visitados = set(cidades.keys())
+    rota = []
+    atual = 'A'
+    nao_visitados.remove(atual)
+    rota.append(atual)
+    
+    while nao_visitados:
+        proxima = min(nao_visitados, key=lambda cidade: distancia(cidades[atual], cidades[cidade]))
+
+        rota.append(proxima)
+        nao_visitados.remove(proxima)
+        atual = proxima
+        
+    rota.append('A')
+    return rota
+
+print("Rota aproximada: ", rota_mais_proxima(cidades))
