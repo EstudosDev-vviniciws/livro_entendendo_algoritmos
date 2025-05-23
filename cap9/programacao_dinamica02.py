4a9cc308b266054cd67b68c1e333a8257128fa20
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-05-23 08:40:00 -0300

    Troquei a variável dp por tabela

--- cap9/programacao_dinamica02.py
+++ cap9/programacao_dinamica02.py
@@ -7,17 +7,17 @@ o prazer total.
 
 def melhor_viagem(tempos, prazeres, tempo_total):
     n = len(tempos)
-    dp = [[0] * (tempo_total + 1) for _ in range(n + 1)]
+    tabela = [[0] * (tempo_total + 1) for _ in range(n + 1)]
     
     for i in range(1, n + 1):
         for t in range(1, tempo_total + 1):
             if tempos[i - 1] <= t:
-                incluir = prazeres[i - 1] + dp[i - 1][t - tempos[i - 1]]
-                excluir = dp[i - 1][t]
-                dp[i][t] = max(incluir, excluir)
+                incluir = prazeres[i - 1] + tabela[i - 1][t - tempos[i - 1]]
+                excluir = tabela[i - 1][t]
+                tabela[i][t] = max(incluir, excluir)
             else:
-                dp[i][t] = dp[i - 1][t]
+                tabela[i][t] = tabela[i - 1][t]
     
-    return dp[n][tempo_total]
+    return tabela[n][tempo_total]
 
 print(melhor_viagem([1, 2, 3], [100, 300, 350], 5))
